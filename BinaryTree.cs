cbd67688561fd80748c92357b1190acea8e0c791
Ivan Ng <ivanng2@hotmail.com>
2021-08-02 08:39:24 +0800

    Binary Tree and node update

---
+++ BinaryTree.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreePayroll
+{
+    class BinaryTree
+    {
+        private Node root;
+
+        public BinaryTree() { }
+
+        public Node gsRoot 
+        {
+            get => root;
+            set => root = value;
+        }
+
+    }
+}
--- Node.cs
+++ Node.cs
@@ -12,10 +12,12 @@ namespace BinaryTreePayroll
         private Node left, right;
 
         public Node() { }
-        public Node(string data) {
+        public Node(string data) 
+        {
             this.data = data;
         }
-        public Node(string data, Node left, Node right) {
+        public Node(string data, Node left, Node right) 
+        {
             this.data = data;
             this.left = left;
             this.right = right;
