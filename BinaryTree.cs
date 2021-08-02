5ec62f3fcf9e87a7697f44222873624847e1498c
Ivan Ng <ivanng2@hotmail.com>
2021-08-02 09:07:18 +0800

    File description added

--- BinaryTree.cs
+++ BinaryTree.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Name: Yang Beng Ng (Ivan)
+//ID: 30031552
+//Description: Payroll system that sorts name
+//Reference: AVL Tree code is sourced from file provided my lecturer on Blackboard
+
 namespace BinaryTreePayroll
 {
     class BinaryTree
@@ -12,11 +17,28 @@ namespace BinaryTreePayroll
 
         public BinaryTree() { }
 
-        public Node gsRoot 
+        public Node gsRoot
         {
             get => root;
             set => root = value;
         }
 
+        private Node AddRecursive(Node cur, Node n)
+        {
+            if (cur == null)
+            {
+                cur = n;
+                return cur;
+            }
+            else if (string.Compare(n.gsData, cur.gsData) < 0)
+            {
+                cur.gsLeft = AddRecursive(cur.gsLeft, n);
+                
+            }
+
+            return cur;
+        }
+
+        private Node 
     }
 }
--- Node.cs
+++ Node.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Name: Yang Beng Ng (Ivan)
+//ID: 30031552
+//Description: Payroll system that sorts name
+//Reference: AVL Tree code is sourced from file provided my lecturer on Blackboard
+
 namespace BinaryTreePayroll
 {
     class Node
@@ -21,6 +26,24 @@ namespace BinaryTreePayroll
             this.data = data;
             this.left = left;
             this.right = right;
+        }
+
+        public string gsData
+        {
+            get => this.data;
+            set => this.data = value;
+        }
+
+        public Node gsLeft
+        { 
+            get => this.left;
+            set => this.left = value;
+        }
+
+        public Node gsRight
+        {
+            get => this.right;
+            set => this.right = value;
         }
     }
 }
